710a3312f626510eb868322fa651f57ba2d380ef
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-13 22:35:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+  # Could also use attr_accessor like below:
+  # attr_accessor :name
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
